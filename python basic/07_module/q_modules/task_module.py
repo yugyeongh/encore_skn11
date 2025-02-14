5965dab3e68f2bd9228a85adfd776174458e1312
yugyeongh <uugyeong27@gmail.com>
2025-02-14 17:51:43 +0900

    python_basic: module

---
+++ python basic/07_module/q_modules/task_module.py
@@ -0,0 +1,5 @@
+def start_task(task_name):
+    return f'작업 {task_name}이(가) 시작되었습니다.'
+
+def complete_task(task_name):
+    return f'작업 {task_name}이(가) 완료되었습니다.'
---
+++ python basic/07_module/q_modules/team_module.py
@@ -0,0 +1,7 @@
+company = '주식회사 Python'
+
+def introduce_manager():
+    return '저는 팀장입니다. 팀을 이끌고 있습니다.'
+
+def introduce_developer():
+    return '저는 개발자입니다. LLM 서비스를 개발하고 있습니다.'
