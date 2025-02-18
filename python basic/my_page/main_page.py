047e7e8a6716d0b60e89be47178097593f65a16a
yugyeongh <uugyeong27@gmail.com>
2025-02-18 11:20:15 +0900

    hotfix: body data crawling

--- python basic/my_page/main_page.py
+++ python basic/my_page/main_page.py
@@ -17,20 +17,40 @@ headers = [th.get_text(strip=True) for th in header.find_all("span", class_="bli
 # st.write(headers)
 
 body = soup.find_all("tbody")[0]
-rows = [th.get_text(strip=True) for th in body.find_all("span")]
-# rows = soup.find_all("tr")[1:] # 첫 번째 tr
-# st.write(rows)
+# st.write(body)
 
 data = []
+
+for b in body.find_all("tr"):
+    rank = body.find('th').text.strip()
+    team = body.find('span', id=lambda x: x and x.startswith('team_')).text.strip()
+
+    for td in soup.find_all('td')[1:]:  # 첫 번째 td는 팀 이름이므로 제외
+        value = td.text.strip()
+        result.append(value)
+
+rows = [th.get_text(strip=True) for th in body.find_all("td")]
+# rows = soup.find_all("tr")[1:] # 첫 번째 tr
+st.write(rows)
+
+data = []
+row_data = []
 rank = 1
 for row in rows:
     if row != '': 
-        data.append(row)
+        row_data.append(row)
     else:
-        data.append(rank)
+        if row_data != []:
+            data.append(row_data)
+            row_data = []
+        row_data.append(rank)
         rank += 1
 st.write(data)
-
+print(len(headers))
+print(len(data))
+df = pd.DataFrame(data)
+st.write(df)
+st.write(headers)
 # df = pd.DataFrame(data, columns=headers)
 
 # st.write(df)
